3a4dc3d6190764eded74b0b1f62cbee8b0469d51
tasnimMoh <92645626+tasnimMoh@users.noreply.github.com>
2021-10-19 23:08:32 +0300

    Add files via upload

---
+++ task2-java.js
@@ -0,0 +1,7 @@
+
+function generatorCat(){
+    var imge=document.createElement('img');
+    var div=document.getElementById('flex-cat-gen');
+    imge.src="https://i.pinimg.com/originals/13/33/95/133395b5a067374035a5efbe0d898667.gif"  ;
+    div.appendChild(imge);
+    }
